1024706c6e790a5275a73de7b976677ab8e63e3f
Garrett Ross <garrettross@gGarrettRoss.local>
2021-08-04 12:19:39 -0500

    working with heroku

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('#btn')
+const container = document.querySelector('.container')
 
 loadEventListeners()
 
@@ -7,8 +8,16 @@ function loadEventListeners() {
 }
 
 async function getData() {
-    const res = await fetch('https://obscure-temple-03891.herokuapp.com/api/students');
+    const res = await fetch('https://obscure-temple-03891.herokuapp.com//api/students')
     const data = await res.json()
-    console.log(data)
+    paintScreen(data)
 }
 
+function paintScreen(data) {
+    for(let i = 0; i < data.length; i++) {
+        let current = data[i]
+        const div = document.createElement('div')
+        div.textContent = current.name
+        container.appendChild(div)
+    }
+}
