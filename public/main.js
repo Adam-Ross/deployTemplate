0ad511566b90f8be47956938cc2bf1966fa3ae64
Garrett Ross <garrettross@gGarrettRoss.local>
2021-08-04 11:43:09 -0500

    proxy added

--- public/main.js
+++ public/main.js
@@ -3,8 +3,12 @@ const btn = document.querySelector('#btn')
 loadEventListeners()
 
 function loadEventListeners() {
-    btn.addEventListener('click', () => {
-        const res = fetch('https://obscure-temple-03891.herokuapp.com/api/students')
-        console.log(res.json())
-    })
+    btn.addEventListener('click', getData)
 }
+
+async function getData() {
+    const res = await fetch('https://obscure-temple-03891.herokuapp.com/api/students');
+    const data = await res.json()
+    console.log(data)
+}
+
