6d4c148fb0747c9d615f21373c82ffb54d14b19a
Garrett Ross <garrettross@gGarrettRoss.local>
2021-08-04 10:12:39 -0500

    Ready to deploy

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const app = express()
+const {Pool} = require('pg')
 
-const PORT = 3004
+const PORT = process.env.PORT || 3004
 
 // Middlewares
 app.use(express.static('public'))
+
+// const pool = new Pool({
+//     host: 'localhost',
+//     user: 'garrettross',
+//     database: 'test_deploy',
+//     port: 5432,
+//     password: ''
+// })
+
+// routes
+app.get('/api/students', async (req, res) => {
+    try {
+        const {rows} = await pool.query('SELECT * FROM student')
+        res.status(200).json(rows)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
